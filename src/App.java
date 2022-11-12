704d4d0e383f8faddbd5cd4254750e1691a3118d
Elvio Junior <elviojunior01@gmail.com>
2022-11-11 23:40:30 -0300

    feat: Modelos iniciais

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso Java");
+        cursoJava.setDescricao("Descrição do Curso Java");
+        cursoJava.setCargaHoraria(8);
+
+        Curso cursoJs = new Curso();
+        cursoJs.setTitulo("Curso JavaScript");
+        cursoJs.setDescricao("Descrição do Curso JavaScript");
+        cursoJs.setCargaHoraria(8);
+
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria Java");
+        mentoriaJava.setDescricao("Descrição da Mentoria Java");
+        mentoriaJava.setData(LocalDate.now());
+
+        Mentoria mentoriaJs = new Mentoria();
+        mentoriaJs.setTitulo("Mentoria JavaScript");
+        mentoriaJs.setDescricao("Descrição da Mentoria JavaScript");
+        mentoriaJs.setData(LocalDate.now());
+
     }
 }
